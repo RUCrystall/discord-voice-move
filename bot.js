7445690269496120d857be65e422a5fd86fa0e43
Igor Shastin <igor.shastin@kofax.com>
2020-05-09 19:11:43 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,35 +1,37 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const isSilent = false;
 
 client.on('ready', () => {
     console.log('discord-voice-move ready. blip-blop!');
 });
 
 client.on('message', message => {
-    if (message.content.startsWith('!move')) {
+    if (message.content.startsWith('!move ')) {
 		let messageMentions = message.mentions.members.array();
 		if (messageMentions.length) {
-			const args = message.content.trim().split(/ +/g);
-			if (args.length > 2) {
-				const channel = getChannelByName(message, args[1]);
-				if (channel) {
-					const membersToMove = messageMentions.filter(m => m.voice.channelID != null && m.voice.channelID != channel.id);
-					if (membersToMove.length) {
-						membersToMove.forEach(m => {
-							m.voice.setChannel(channel);
-						});
-					} else if (!isSilent) {
-						message.reply('No members to move.');
+			message.content.substring(6).split('||').forEach(opts => {
+				const args = opts.trim().split(/ +/g);
+				if (args.length == 2) {
+					const channel = getChannelByName(message, args[0]);
+					console.log(args[1]);
+					if (channel) {
+						const membersToMove = messageMentions.filter(m => m.voice.channelID != null && m.voice.channelID != channel.id);
+						if (membersToMove.length) {
+							membersToMove.forEach(m => {
+								m.voice.setChannel(channel);
+							});
+						} else {
+							log('No members to move.');
+						}
+					} else {
+						log('Channel not found.');
 					}
-				} else if (!isSilent) {
-					message.reply('Channel not found.');
+				} else {
+					log('Not all arguments provided.');
 				}
-			} else if (!isSilent) {
-				message.reply('Not all arguments provided.');
-			}
-		} else if (!isSilent) {
-			message.reply('There are no mentions in the message.');
+			});
+		} else {
+			log('There are no mentions in the message.');
 		}
     }
 });
@@ -41,6 +43,12 @@ function getChannelByName(message, name) {
 		voiceChannel = channelCache.find(c => c.name.toLowerCase() === name.toLowerCase());
 	}
 	return voiceChannel;
+}
+
+function log(message, content) {
+	if (process.env.DEBUG) {
+		message.reply(content);
+	}
 }
 
 // THIS MUST BE THIS WAY
