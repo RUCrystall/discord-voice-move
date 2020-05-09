0956f7386b107ecf4a727c833cd13df4c789a482
Igor Shastin <igor.shastin@kofax.com>
2020-05-09 16:53:29 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,9 +38,9 @@ client.on('message', message => {
 
 function getChannelByName(message, name) {
 	const channelCache = message.guild.channels.cache.array().filter(c => c.type === 'voice');
-	console.log(channelCache);
 	let voiceChannel = channelCache.find(c => c.id === name);
 	if (voiceChannel === null) {
+		console.log(channelCache.find(c => c.name.toLowerCase() === name.toLowerCase()));
 		voiceChannel = channelCache.find(c => c.name.toLowerCase() === name.toLowerCase());
 	}
 	return voiceChannel;
